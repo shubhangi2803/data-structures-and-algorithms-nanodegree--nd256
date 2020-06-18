a3e4d5c696803817b966dc0e403a8a51a3139333
Shubhangi Bhargava <54638807+shubhangi2803@users.noreply.github.com>
2020-06-19 00:07:20 +0530

    initial commit

---
+++ DATA STRUCTURES/Recursion/Palindrome.py
@@ -0,0 +1,23 @@
+def is_palindrome(input):
+    """
+    Return True if input is palindrome, False otherwise.
+    
+    Args:
+       input(str): input to be checked if it is palindrome
+    """
+    if len(input)<=1:
+        return True
+    else:
+        first=input[0]
+        last=input[-1]
+        sub=input[1:-1]
+        return (first==last) and is_palindrome(sub)
+
+# Test Cases
+
+print ("Pass" if  (is_palindrome("")) else "Fail")
+print ("Pass" if  (is_palindrome("a")) else "Fail")
+print ("Pass" if  (is_palindrome("madam")) else "Fail")
+print ("Pass" if  (is_palindrome("abba")) else "Fail")
+print ("Pass" if not (is_palindrome("Udacity")) else "Fail")
+
