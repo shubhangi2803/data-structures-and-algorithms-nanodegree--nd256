334acc8e005ad884d63d612980b5c57ff002fc38
Shubhangi Bhargava <54638807+shubhangi2803@users.noreply.github.com>
2020-06-21 14:12:07 +0530

    initial commit

---
+++ DATA STRUCTURES/Stack and Queue/DS-QueueUsingLinkedList.py
@@ -0,0 +1,89 @@
+class Node:
+    def __init__(self,value):
+        self.value=value
+        self.next=None
+
+class Queue:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+        self.num_elements=0
+
+    def enqueue(self,value):
+        if self.num_elements==0:
+            self.head=Node(value)
+            self.tail=self.head
+        else:
+            temp=Node(value)
+            self.tail.next=temp
+            temp=self.tail
+        self.num_elements+=1
+
+    def size(self):
+        return self.num_elements
+    
+    def is_empty(self):
+        return self.size()==0
+
+    def dequeue(self):
+        if self.is_empty():
+            return True
+        x=self.head.value
+        self.head=self.head.next
+        self.num_elements-=1
+        return x
+
+# Setup
+q = Queue()
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+
+# Test size
+print ("Pass" if (q.size() == 3) else "Fail")
+
+# Test dequeue
+print ("Pass" if (q.dequeue() == 1) else "Fail")
+
+# Test enqueue
+q.enqueue(4)
+print ("Pass" if (q.dequeue() == 2) else "Fail")
+print ("Pass" if (q.dequeue() == 3) else "Fail")
+print ("Pass" if (q.dequeue() == 4) else "Fail")
+q.enqueue(5)
+print ("Pass" if (q.size() == 1) else "Fail")
+
+
+
+##/////////////////////////////////////////////////////////
+##class Queue:
+##    
+##    def __init__(self):
+##        self.head = None
+##        self.tail = None
+##        self.num_elements = 0
+##        
+##    def enqueue(self, value):
+##        new_node = Node(value)
+##        if self.head is None:
+##            self.head = new_node
+##            self.tail = self.head
+##        else:
+##            self.tail.next = new_node    # add data to the next attribute of the tail (i.e. the end of the queue)
+##            self.tail = self.tail.next   # shift the tail (i.e., the back of the queue)
+##        self.num_elements += 1
+##            
+##    def dequeue(self):
+##        if self.is_empty():
+##            return None
+##        value = self.head.value          # copy the value to a local variable
+##        self.head = self.head.next       # shift the head (i.e., the front of the queue)
+##        self.num_elements -= 1
+##        return value
+##    
+##    def size(self):
+##        return self.num_elements
+##    
+##    def is_empty(self):
+##        return self.num_elements == 0
+        
