82c8c7951126f86fc7acc3e86cd5a61d1e910c87
Shubhangi Bhargava <54638807+shubhangi2803@users.noreply.github.com>
2020-07-07 22:41:42 +0530

    Initial commit

---
+++ DATA STRUCTURES/Tree/BFS traversal.py
@@ -0,0 +1,102 @@
+from collections import deque
+
+class Node(object):
+        
+    def __init__(self,value = None):
+        self.value = value
+        self.left = None
+        self.right = None
+        
+    def set_value(self,value):
+        self.value = value
+        
+    def get_value(self):
+        return self.value
+        
+    def set_left_child(self,left):
+        self.left = left
+        
+    def set_right_child(self, right):
+        self.right = right
+        
+    def get_left_child(self):
+        return self.left
+    
+    def get_right_child(self):
+        return self.right
+
+    def has_left_child(self):
+        return self.left != None
+    
+    def has_right_child(self):
+        return self.right != None
+    
+    # define __repr_ to decide what a print statement displays for a Node object
+    def __repr__(self):
+        return f"Node({self.get_value()})"
+    
+    def __str__(self):
+        return f"Node({self.get_value()})"
+    
+    
+class Tree():
+    def __init__(self, value=None):
+        self.root = Node(value)
+        
+    def get_root(self):
+        return self.root
+
+tree = Tree("apple")
+tree.get_root().set_left_child(Node("banana"))
+tree.get_root().set_right_child(Node("cherry"))
+tree.get_root().get_left_child().set_left_child(Node("dates"))
+
+class Queue():
+    def __init__(self):
+        self.q = deque()
+        
+    def enq(self,value):
+        self.q.appendleft(value)
+        
+    def deq(self):
+        if len(self.q) > 0:
+            return self.q.pop()
+        else:
+            return None
+    
+    def __len__(self):
+        return len(self.q)
+    
+    def __repr__(self):
+        if len(self.q) > 0:
+            s = "<enqueue here>\n_________________\n" 
+            s += "\n_________________\n".join([str(item) for item in self.q])
+            s += "\n_________________\n<dequeue here>"
+            return s
+        else:
+            return "<queue is empty>"
+
+
+# BFS algorithm
+def bfs(tree):
+    q=Queue()
+    visit_order=[]
+    root=tree.get_root()
+    q.enq(root)
+    while q.__len__()>0:
+        node=q.deq()
+        visit_order.append(node.get_value())
+        if node.has_left_child():
+            q.enq(node.get_left_child())
+        if node.has_right_child():
+            q.enq(node.get_right_child())
+    return visit_order
+
+tree = Tree("apple")
+tree.get_root().set_left_child(Node("banana"))
+tree.get_root().set_right_child(Node("cherry"))
+tree.get_root().get_left_child().set_left_child(Node("dates"))
+
+# check solution: should be: apple, banana, cherry, dates
+ans=bfs(tree)
+print(ans)
